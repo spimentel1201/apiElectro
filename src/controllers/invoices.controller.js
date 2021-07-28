6ba85b86d6a307e77b2c1caae6482d5a61ac063b
SonnyPM <spimentelm1201@gmail.com>
2021-07-28 00:17:58 -0500

    Find by Code

--- src/controllers/invoices.controller.js
+++ src/controllers/invoices.controller.js
@@ -20,6 +20,13 @@ export const getInvoiceById = async (req,res) => {
     res.status(200).json(invoice)
 }
 
+export const getInvoiceByCodigo = async (req,res) => {
+    //const {invoicecode} = req.params;
+    const invoice = await Invoice.find({codigo: req.params.invoiceId}).exec();
+    console.log(invoice)
+    res.status(200).json(invoice)
+}
+
 export const updateInvoiceById = async (req,res) => {
     const updateInvoice = await Invoice.findByIdAndUpdate(req.params.invoiceId, req.body, {
         new: true
--- src/routes/invoice.routes.js
+++ src/routes/invoice.routes.js
@@ -5,7 +5,8 @@ import * as invoicesCtrl from '../controllers/invoices.controller'
 import {authJwt} from '../middlewares'
 router.post('/',[authJwt.verifyToken,authJwt.isWorker], invoicesCtrl.createInvoice)
 router.get('/', invoicesCtrl.getInvoices)
-router.get('/:inoviceId', invoicesCtrl.getInvoiceById)
+router.get('/:invoiceId', invoicesCtrl.getInvoiceById)
+router.get('/code/:invoiceId', invoicesCtrl.getInvoiceByCodigo)
 router.put('/:invoiceId',[authJwt.verifyToken,authJwt.isWorker],authJwt.verifyToken, invoicesCtrl.updateInvoiceById)
 router.delete('/:invoiceId', [authJwt.verifyToken,authJwt.isWorker],invoicesCtrl.deleteInvoiceById)
 
