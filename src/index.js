00ad2bcd23472f9af365311d07eb9020e811ff01
Sonny <spimentelm1201@gmail.com>
2021-12-13 09:40:48 -0500

    cambios port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import app from './app'
 import './database'
-app.listen(process.env.ROOT || 5000);
-console.log("Server listen on port",5000)
+const host = '0.0.0.0';
+const port = process.env.PORT || 5000;
+app.listen(port, host);
+console.log("Server listen on port", 5000)
